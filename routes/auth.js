e1b0617b4ac87030f026f88624b58bdcfc1ffc8d
Frankie Riviera <frankgriviera@outlook.com>
2020-11-18 01:25:50 -0500

    added the middleware route as a standard route so frontend can auth onfirm token when a component loads

--- routes/auth.js
+++ routes/auth.js
@@ -48,5 +48,35 @@ router.post("/login", async (req,res)=>{
     }
 });
 
+// /auth route for Frontend to double check auth whenever it loads a component
+router.post("/", async (req,res)=>{
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).json({detail:'Unauthorized, no auth token provided'})
+
+    try{
+        let axiosConfig = getAxiosConfig(token);
+        let response = await axios.post(baseUrl+"auth",{}, axiosConfig );
+        req.user = response.data
+        next();
+    }catch(ex){
+        if (ex.response){
+            return res.status(400).json({detail:ex.response.data})
+        }else{
+            return res.status(400).json({detail:JSON.stringify(ex)})
+        }
+    }
+});
+
+function getAxiosConfig(token){
+    let axiosConfig = {
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+            "x-auth-token":token
+        }
+    };
+    return axiosConfig;
+}
+
 module.exports=router;
 
